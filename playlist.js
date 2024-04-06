df1285728f4710466bd3f696086e08cb6654ae34
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-04-06 20:54:32 +0100

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,1 +1,51 @@
+window.onload=()=>{
+    
+    let next = document.querySelector(".next");
+   let shuffle = document.querySelector(".shuf");
+   let prev = document.querySelector(".prev")
+  let share =  document.querySelector(".share")  
+let songlist__btn=  document.querySelector(".songlist__btn")  
+let songs =  document.querySelector(".songs")  
 
+
+const playList = {
+favorite:[
+{
+     artiste:"wizkid",
+    song :"baba nla",
+    duration:4, 
+     
+},{
+     artiste:"davido",
+    song :"FIA",
+    duration:3.5,   
+},{
+     artiste:"burna boy",
+    song :"won da mo",
+    duration:5,   
+}
+],
+frequentlyPlayed:[
+{
+     artiste:"Nathaniel Bassey",
+    song :"IBA",
+    duration:6,   
+},{
+     artiste:"davido",
+    song :"FEM",
+    duration:4.5,   
+},{
+     artiste:"burna boy",
+    song :"last last",
+    duration:5.6,   
+}
+]
+
+    
+    
+    
+}
+    
+    
+    
+  
